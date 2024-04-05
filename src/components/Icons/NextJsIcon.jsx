55cf67af4ab4cd6ce25c2185eb61864c4d8c7ce9
yurdigrfnn <yurdiansyah143@gmail.com>
2024-04-06 04:18:34 +0700

    features: ✨Skills Page

---
+++ src/components/Icons/NextJsIcon.jsx
@@ -0,0 +1,72 @@
+export default function NextJsIcon() {
+    return (
+      <svg
+        width="64"
+        height="64"
+        viewBox="0 0 64 64"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clipPath="url(#clip0_327_2228)">
+          <mask
+            id="mask0_327_2228"
+            style={{ maskType: 'alpha' }}
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="0"
+            width="64"
+            height="64"
+          >
+            <path
+              d="M32 64C49.6731 64 64 49.6731 64 32C64 14.3269 49.6731 0 32 0C14.3269 0 0 14.3269 0 32C0 49.6731 14.3269 64 32 64Z"
+              fill="black"
+            />
+          </mask>
+          <g mask="url(#mask0_327_2228)">
+            <path
+              d="M32.0002 62.9333C49.0842 62.9333 62.9336 49.084 62.9336 32C62.9336 14.916 49.0842 1.06665 32.0002 1.06665C14.9162 1.06665 1.06689 14.916 1.06689 32C1.06689 49.084 14.9162 62.9333 32.0002 62.9333Z"
+              fill="black"
+              stroke="white"
+              strokeWidth="6"
+            />
+            <path
+              d="M53.1586 56.0071L24.584 19.2H19.2002V44.7893H23.5073V24.6697L49.7776 58.6115C50.963 57.8183 52.0923 56.9475 53.1586 56.0071Z"
+              fill="url(#paint0_linear_327_2228)"
+            />
+            <path
+              d="M45.1558 19.2H40.8892V44.8H45.1558V19.2Z"
+              fill="url(#paint1_linear_327_2228)"
+            />
+          </g>
+        </g>
+        <defs>
+          <linearGradient
+            id="paint0_linear_327_2228"
+            x1="38.7558"
+            y1="41.4222"
+            x2="51.378"
+            y2="57.0666"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="white" />
+            <stop offset="1" stopColor="white" stopOpacity="0" />
+          </linearGradient>
+          <linearGradient
+            id="paint1_linear_327_2228"
+            x1="43.0225"
+            y1="19.2"
+            x2="42.951"
+            y2="38"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="white" />
+            <stop offset="1" stopColor="white" stopOpacity="0" />
+          </linearGradient>
+          <clipPath id="clip0_327_2228">
+            <rect width="64" height="64" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    );
+  }
+  
--- src/features/home/components/Home.jsx
+++ src/features/home/components/Home.jsx
@@ -1,41 +1,119 @@
+import GitIcon from "../../../components/Icons/GitIcon";
 import GithubIcon from "../../../components/Icons/GithubIcon";
+import JavaScriptIcon from "../../../components/Icons/JavaScriptIcon";
+import LaravelIcon from "../../../components/Icons/LaravelIcon";
+import MysqlIcon from "../../../components/Icons/MysqlIcon";
+import NextJsIcon from "../../../components/Icons/NextJsIcon";
+import NodeIcon from "../../../components/Icons/NodeIcon";
+import PhpIcon from "../../../components/Icons/PhpIcon";
 import PinIcon from "../../../components/Icons/PinIcon";
+import ReactIcon from "../../../components/Icons/ReactIcon";
+import ReduxIcon from "../../../components/Icons/ReduxIcon";
+import TailwindCSSIcon from "../../../components/Icons/TailwindCSSIcon";
+import VueIcon from "../../../components/Icons/VueIcon";
 
 export const Home = () => {
   return (
-    <div className="mx-auto w-11/12 sm:w-10/12 mt-24 dark:text-gray-300">
-      <h1 className="font-bold sm:text-6xl text-4xl">Hi,Im Yurdiansyah</h1>
-      <p className="mt-5">
-        {`I'm a Frontend Developer passionate about creating engaging and
+    <div>
+      {/* Home page */}
+      <div>
+        <div className="mx-auto w-11/12 sm:w-10/12 mt-24 dark:text-gray-300">
+          <h1 className="font-bold sm:text-6xl text-4xl">Hi,Im Yurdiansyah</h1>
+          <p className="mt-5">
+            {`I'm a Frontend Developer passionate about creating engaging and
         user-friendly websites. Through my work, I aim to blend design and
         functionality seamlessly to deliver exceptional digital experiences. In
         this portfolio, you'll find a collection of projects where I've applied
         my skills in frontend development, crafting responsive layouts and
         enhancing user interactions to make the web more accessible and
-        enjoyable for everyone. Thank you for taking the time to explore my
-        portfolio; I look forward to discussing how we can collaborate to
-        achieve your digital goals. Let's build something amazing together!`}
-      </p>
-      <div className="my-10 flex flex-col gap-4">
-        <span className="flex gap-3">
-          <PinIcon className="my-auto" />
-          <span>Bogor, indonesia</span>
-        </span>
-        <span className="flex gap-3 ">
-          <span className="ml-1  w-3 h-3 rounded-full bg-green-600 my-auto relative">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-600 opacity-75"></span>
-          </span>
-          <span className="ml-1 ">Avaiable For fulltime job</span>
-        </span>
+        enjoyable for everyone`}
+          </p>
+          <div className="my-10 flex flex-col gap-4">
+            <span className="flex gap-3">
+              <PinIcon className="my-auto" />
+              <span>Bogor, indonesia</span>
+            </span>
+            <span className="flex gap-3 ">
+              <span className="ml-1  w-3 h-3 rounded-full bg-green-600 my-auto relative">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-600 opacity-75"></span>
+              </span>
+              <span className="ml-1 ">Avaiable For fulltime job</span>
+            </span>
+          </div>
+          <div className="mb-5">
+            <a
+              href="http://github.com/yurdigrfnn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GithubIcon className="dark:text-gray-300 " />
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="mb-5">
-        <a
-          href="http://github.com/yurdigrfnn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GithubIcon className="dark:text-gray-300 " />
-        </a>
+
+      {/* skils */}
+      <div className="dark:bg-gray-900 bg-gray-50">
+        <div className="mx-auto w-11/12 sm:w-10/12 py-16 sm:py-24 mt-24 dark:text-gray-300">
+          <div className="flex flex-col gap-4 ">
+            <span className="flex justify-center">
+              <h2 className="dark:text-white bg-gray-400 dark:bg-gray-600 py-2 text-center my-auto px-6 rounded-xl text-sm font-medium">
+                get In touch
+              </h2>
+            </span>
+            <span className="flex justify-center">
+              <p className="text-xl text-center">
+                The skills, tools and technologies I am really good at:
+              </p>
+            </span>
+          </div>
+          <div className="mt-6 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7  gap-5">
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <JavaScriptIcon />
+              <p>JavaScript</p>
+            </div>
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <ReactIcon />
+              <p>React Js</p>
+            </div>
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <TailwindCSSIcon />
+              <p>Tailwind CSS</p>
+            </div>
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <NodeIcon />
+              <p>Node JS</p>
+            </div>
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <ReduxIcon />
+              <p>Redux</p>
+            </div>
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <PhpIcon />
+              <p>Php</p>
+            </div>
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <MysqlIcon />
+              <p>MySql</p>
+            </div>
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <LaravelIcon />
+              <p>Laravel</p>
+            </div>
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <VueIcon />
+              <p>Vue</p>
+            </div>
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <NextJsIcon />
+              <p>Next JS</p>
+            </div>
+            <div className="flex flex-col gap-2 items-center justify-center rounded-md">
+              <GitIcon />
+              <p>Git</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
